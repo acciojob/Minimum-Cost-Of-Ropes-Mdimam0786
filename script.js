32b0bba2491df7bbfe3a8d9c3d6340ca629a5285
Mdimam0786 <142109698+Mdimam0786@users.noreply.github.com>
2023-10-26 01:49:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-
-
-
 function calculateMinCost() {
-  const ropeLengthsStr = document.getElementById('rope-lengths').value;
-  const ropeLengthsArr = ropeLengthsStr.split(',').map(Number);
-  const n = ropeLengthsArr.length;
-
-  let pq = [];
-  for (let i = 0; i < n; i++) {
-    pq.push(ropeLengthsArr[i]);
+  //your code here
+ 
+  // Get the input from the form
+  const input = document.getElementById("rope-lengths").value;
+  
+  // Convert the comma-separated string to an array of integers
+  const ropeLengths = input.split(",").map((length) => parseInt(length));
+  
+  // Sort the array of rope lengths in non-decreasing order
+  ropeLengths.sort((a, b) => a - b);
+  
+  // Initialize the minimum cost and total cost to 0
+  let minCost = 0;
+  let totalCost = 0;
+  
+  // Keep connecting the two shortest ropes until there is only one rope left
+  while (ropeLengths.length > 1) {
+    // Take the two shortest ropes
+    const shortestRope1 = ropeLengths.shift();
+    const shortestRope2 = ropeLengths.shift();
+    
+    // Calculate the cost of connecting the two shortest ropes
+    const cost = shortestRope1 + shortestRope2;
+    
+    // Update the minimum cost and total cost
+    minCost += cost;
+    totalCost += cost;
+    
+    // Add the new rope to the array of rope lengths
+    ropeLengths.push(cost);
+    
+    // Sort the array of rope lengths in non-decreasing order
+    ropeLengths.sort((a, b) => a - b);
   }
-  pq.sort(function (a, b) { return a - b; });
-
-  let res = 0;
-  while (pq.length > 1) {
-    let first = pq.shift();
-    let second = pq.shift();
-    res += first + second;
-    pq.push(first + second);
-    pq.sort(function (a, b) { return a - b; });
-  }
-
-  document.getElementById('result').textContent = res;
-}  
+  
+  // Display the minimum cost in the result div
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = `The minimum cost of connecting the ropes is ${minCost}.`;
+}
